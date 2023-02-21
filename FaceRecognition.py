62e11322e3a9b3864e923630c3705c6709b06d83
thuggeelya <ilya-fdmdh@mail.ru>
2023-02-21 16:36:11 +0700

    Refactoring #1

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,14 +1,15 @@
 # Youtube Tutorial - https://youtu.be/RrE7Ipr93SA
 
 # Importing all modules
-import face_recognition as fr # To recognize faces
-import numpy as np # To handle all lists/arrays
-import cv2 # To capture webcam footage
-import os # To handle all matters relating to folders, paths, image/file names, etc.
+import face_recognition as fr  # To recognize faces
+import numpy as np  # To handle all lists/arrays
+import cv2  # To capture webcam footage
+import os  # To handle all matters relating to folders, paths, image/file names, etc.
 
 # Path to folder containing all known faces. The 'known' folder needs all known faces, 
 # where the image\file name is the name of the person to be recognized.
-faces_path = "C:\\path\\to\\folder\\with\\known\\faces"
+faces_path = "D:\\thuggeelya\\Desktop\\8ПМ2Л_2\\ММРО\\1\\Lab1\\faces"
+
 
 # Function to get face names, as well as face encodings
 def get_face_encodings():
@@ -22,9 +23,10 @@ def get_face_encodings():
         face = fr.load_image_file(f"{faces_path}\\{name}")
         face_encodings.append(fr.face_encodings(face)[0])
 
-        face_names[i] = name.split(".")[0] # To remove ".jpg" or any other image extension
-    
+        face_names[i] = name.replace('_', ' ').split(".")[0]  # To remove ".png" or any other image extension
+
     return face_encodings, face_names
+
 
 # Retrieving face encodings and storing them in the face_encodings variable, along with the names
 face_encodings, face_names = get_face_encodings()
@@ -40,7 +42,7 @@ while True:
     success, image = video.read()
 
     # Making current frame smaller so program runs faster
-    resized_image = cv2.resize(image, (int(image.shape[1]/scl), int(image.shape[0]/scl)))
+    resized_image = cv2.resize(image, (int(image.shape[1] / scl), int(image.shape[0] / scl)))
 
     # Converting current frame to RGB, since that's what the face recognition module uses
     rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
@@ -62,11 +64,11 @@ while True:
             top, right, bottom, left = face_location
 
             # Drawing rectangle around face
-            cv2.rectangle(image, (left*scl, top*scl), (right*scl, bottom*scl), (0, 0, 255), 2)
+            cv2.rectangle(image, (left * scl, top * scl), (right * scl, bottom * scl), (0, 0, 255), 2)
 
             # Setting font, as well as displaying text of name
             font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(image, name, (left*scl, bottom*scl + 20), font, 0.8, (255, 255, 255), 1)
+            cv2.putText(image, name, (left * scl, bottom * scl + 20), font, 0.8, (255, 255, 255), 1)
 
     # Displaying final image on the screen
     cv2.imshow("frame", image)
